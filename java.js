1c7bd3974665a87c5989a88b10c94606b558a493
John Castillo <JohnluisCastillo@gmail.com>
2017-12-05 11:44:25 -0500

    first file

--- java.js
+++ java.js
@@ -1,1 +1,34 @@
-console.log('hey');
+function fizzBuzz(num) {
+    for (let i = 1; i <= num; i++) {
+        switch (true) {
+            case i % 15 === 0:
+                console.log('FizzBuzz');
+                break;
+            case i % 3 === 0:
+                console.log('Fizz');
+                break;
+            case i % 5 === 0:
+                console.log('Buzz');
+                break;
+            default:
+                console.log(i);
+                break;
+        }
+    }
+}
+
+fizzBuzz(15);
+
+
+function average(numbers) {
+    let total = numbers[0];
+   
+    numbers.forEach(()=>{
+      total += numbers
+      console.log(numbers)
+    })
+  
+  return total / numbers.length;
+  }
+
+  average([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
