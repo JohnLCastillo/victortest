f14f28885d27103e9e59f7ffb556e827046fe35f
John Castillo <JohnluisCastillo@gmail.com>
2017-12-05 13:35:19 -0500

    finished part 1

--- java.js
+++ java.js
@@ -1,67 +1,75 @@
-//Max and min drill 
+// //Max and min drill 
 
-const numbers = [2, 13, 4, 11, 5, 7, 9];
+// const numbers = [2, 13, 4, 11, 5, 7, 9];
 
-function max(numbers) {
-    let i = 0;
-    let myCounter = numbers[0];
-    while (i < numbers.length) {
-        if (numbers[i] > myCounter) {
-            myCounter = numbers[i];
-        }
-        i++;
-    }
-    console.log(myCounter);
-}
+// function max(numbers) {
+//     let i = 0;
+//     let myCounter = numbers[0];
+//     while (i < numbers.length) {
+//         if (numbers[i] > myCounter) {
+//             myCounter = numbers[i];
+//         }
+//         i++;
+//     }
+//     console.log(myCounter);
+// }
 
-max(numbers);
+// max(numbers);
 
-function min(numbers) {
-    let i = 0;
-    let myMin = numbers[0];
-    while (i < numbers.length) {
-        if (numbers[i] < myMin) {
-            myMin = numbers[i];
-        }
-        i++;
-    }
-    console.log(myMin);
-}
+// function min(numbers) {
+//     let i = 0;
+//     let myMin = numbers[0];
+//     while (i < numbers.length) {
+//         if (numbers[i] < myMin) {
+//             myMin = numbers[i];
+//         }
+//         i++;
+//     }
+//     console.log(myMin);
+// }
 
-min(numbers);
+// min(numbers);
 
 
-function fizzBuzz(num) {
-    for (let i = 1; i <= num; i++) {
-        switch (true) {
-            case i % 15 === 0:
-                console.log('FizzBuzz');
-                break;
-            case i % 3 === 0:
-                console.log('Fizz');
-                break;
-            case i % 5 === 0:
-                console.log('Buzz');
-                break;
-            default:
-                console.log(i);
-                break;
-        }
-    }
-}
+// function fizzBuzz(num) {
+//     for (let i = 1; i <= num; i++) {
+//         switch (true) {
+//             case i % 15 === 0:
+//                 console.log('FizzBuzz');
+//                 break;
+//             case i % 3 === 0:
+//                 console.log('Fizz');
+//                 break;
+//             case i % 5 === 0:
+//                 console.log('Buzz');
+//                 break;
+//             default:
+//                 console.log(i);
+//                 break;
+//         }
+//     }
+// }
 
-fizzBuzz(15);
+// fizzBuzz(15);
 
+// function average(numbers) {
+//   let total = numbers[0];
+//   let i = 1
+//   while (i < numbers.length) {
+//     total += numbers[i];
+//     i++;
+//   }
+//   console.log(total / numbers.length);
+// }
 
 function average(numbers) {
     let total = numbers[0];
-   
-    numbers.forEach(()=>{
-      total += numbers
-      console.log(numbers)
-    })
-  
-  return total / numbers.length;
+
+    numbers.forEach(function(i){
+      total += i;
+    });
+  // console.log(total);
+  console.log(total / numbers.length);
   }
 
-  average([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+average([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
